3a3216313b86f1a285ffeda228fb283a9b16e3a0
Todor Chakarov <tosheee@abv.bg>
2021-05-01 10:29:03 +0300

    Update scraper

--- scraper/scraper/mysql_connection.py
+++ scraper/scraper/mysql_connection.py
@@ -1,14 +1,9 @@
 import pymysql
-
+from dbconfig import mysql_prop
 
 class MysqlConnection(object):
     def __init__(self):
-        self.conn = pymysql.connect(host='localhost',
-                                    port=8889,
-                                    user='root',
-                                    password='root',
-                                    db='scraper')
-
+        self.conn = pymysql.connect(**mysql_prop)
         self.cursor = self.conn.cursor()
 
     def close_spider(self):
