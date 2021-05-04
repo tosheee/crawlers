16a6d984823286a0251ef509e008089327bc0bd7
Todor Chakarov <tosheee@abv.bg>
2021-05-04 08:36:21 +0300

    Update project

--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -3,5 +3,5 @@ import pkgutil
 BOT_NAME = 'scraper'
 SPIDER_MODULES = ['scraper.spiders']
 NEWSPIDER_MODULE = 'scraper.spiders'
-settings = pkgutil.get_loader('scrapy_settings.settings')
+settings = pkgutil.get_loader('crawler_settings')
 exec(compile(open(settings.get_filename(), "rb").read(), settings.get_filename(), 'exec'))
